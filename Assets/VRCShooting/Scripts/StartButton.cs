f71e170b886e8daf3b47cf8dc29ead549dbd617e
Mushus <6138982+Mushus@users.noreply.github.com>
2020-06-24 00:47:29 +0900

    必要そうなスクリプトとオブジェクト用意

---
+++ Assets/VRCShooting/Scripts/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UdonSharp;
+using UnityEngine.UI;
+
+public class StartButton : UdonSharpBehaviour
+{
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+    }
+
+    public override void Interact()
+    {
+        gameObject.SetActive(false);
+        // TODO: 検知されてるユーザーを BattleField 上に飛ばす処理
+        // GameObject.FindGameObjectsWithTag("Respawn");
+    }
+}
---
+++ Assets/VRCShooting/Scripts/UserDetector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UdonSharp;
+using UnityEngine.UI;
+using VRC.SDKBase;
+
+public class UserDetector : UdonSharpBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    private Transform cashedTransform;
+
+    private void Start()
+    {
+        cashedTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        // transform.Rotate(Vector3.up, 90f * Time.deltaTime);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // TODO: 呼び出されない原因を探る
+        if (other.GetType() == typeof(CharacterController))
+        {
+            var player = Networking.LocalPlayer;
+            player.SetVelocity(speed * cashedTransform.up);
+            // var targetPos = new Vector3(1.0f,0.0f,0.0f);
+            // var targetRot = player.GetRotation();
+            // player.TeleportTo(targetPos, targetRot);
+        }
+    }
+}
