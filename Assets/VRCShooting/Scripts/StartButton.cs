022cb2d95fbb81274793a6319cf38981cf355ea4
Mushus <6138982+Mushus@users.noreply.github.com>
2020-06-24 09:50:47 +0900

    ボタンを押したキャラをバトルフィールドに転送するように

--- Assets/VRCShooting/Scripts/StartButton.cs
+++ Assets/VRCShooting/Scripts/StartButton.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
-using UdonSharp;
 using UnityEngine.UI;
+using UdonSharp;
+using VRC.SDKBase;
 
 public class StartButton : UdonSharpBehaviour
 {
+    [SerializeField]
+    private GameObject teleportFrom = null;
+
+    [SerializeField]
+    private GameObject teleportTo = null;
     private void Update()
     {
         transform.Rotate(Vector3.up, 90f * Time.deltaTime);
@@ -11,8 +17,20 @@ public class StartButton : UdonSharpBehaviour
 
     public override void Interact()
     {
-        gameObject.SetActive(false);
+        if (teleportFrom == null || teleportTo == null) {
+            return;
+        }
+
         // TODO: 検知されてるユーザーを BattleField 上に飛ばす処理
-        // GameObject.FindGameObjectsWithTag("Respawn");
+        var player = Networking.LocalPlayer;
+        var playerPos = player.GetPosition();
+        var playerRot = player.GetRotation();
+
+        var teleportFromPos = teleportFrom.transform.position;
+        var teleportToPos = teleportTo.transform.position;
+        var targetVector = teleportToPos - teleportFromPos;
+        
+        var rotation = player.GetRotation();
+        player.TeleportTo(playerPos + targetVector, playerRot);
     }
 }
