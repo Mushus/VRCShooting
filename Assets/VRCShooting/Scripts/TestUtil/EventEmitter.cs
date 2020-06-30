23995da9aa19f7a5ecd6cd743de3dbedf0d81722
mushus <6138982+Mushus@users.noreply.github.com>
2020-06-30 09:01:14 +0900

    イベントを実行するユーティリティ追加、ハンドガンの持ち方修正

---
+++ Assets/VRCShooting/Scripts/TestUtil/EventEmitter.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+// EventEmitter is a button that emits a custom event for a given GameObject
+public class EventEmitter : UdonSharpBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private string eventName;
+
+    public override void Interact()
+    {
+        var targetUdonBehaviour = (UdonBehaviour)target.GetComponent(typeof(UdonBehaviour));
+        targetUdonBehaviour.SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, eventName);
+    }
+}
