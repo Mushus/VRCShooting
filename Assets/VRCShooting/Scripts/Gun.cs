9125be3fad7ea334ac21dd05f874af3258b78dde
Hibikine Kage <goodsmile314159@gmail.com>
2020-07-06 22:02:08 +0900

    銃の弾数同期を組んだ

--- Assets/VRCShooting/Scripts/Gun.cs
+++ Assets/VRCShooting/Scripts/Gun.cs
@@ -19,7 +19,6 @@ public class Gun : UdonSharpBehaviour
     [SerializeField] private AudioSource reloadSound;
     /// Self transform
     private Transform _transform;
-    private GameObject _gameObject;
     private int _coolTime = 0;
     /// number of bullets in the magazine
     private int _bulletsInMagazine = 17;
@@ -31,11 +30,13 @@ public class Gun : UdonSharpBehaviour
     private void Start()
     {
         _transform = GetComponent<Transform>();
+        Init();
     }
     private void Update()
     {
         _coolTime -= 1;
         if (_isPickup && _coolTime <= 0 && Input.GetKeyDown(KeyCode.R)) {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Reload");
             Reload();
         }
     }
@@ -43,7 +44,7 @@ public class Gun : UdonSharpBehaviour
     public override void OnPickup()
     {
         _isPickup = true;
-        Networking.SetOwner(Networking.LocalPlayer, _gameObject);
+        Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
     }
 
     public override void OnDrop()
@@ -54,7 +55,7 @@ public class Gun : UdonSharpBehaviour
         for(var i = 0; i < 64; ++i) {
             var master = VRCPlayerApi.GetPlayerById(i);
             if (master != null) {
-                Networking.SetOwner(master, _gameObject);
+                Networking.SetOwner(master, this.gameObject);
                 break;
             }
         }
@@ -66,6 +67,7 @@ public class Gun : UdonSharpBehaviour
             return;
         }
         if (_bulletsInMagazine <= 0) {
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Reload");
             Reload();
             return;
         }
@@ -79,6 +81,12 @@ public class Gun : UdonSharpBehaviour
         }
 
         _coolTime = coolTime;
+        this.SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnShot");
+        OnShot();
+    }
+
+    private void OnShot()
+    {
         _bulletsInMagazine -= 1;
         _isReloadable = true;
     }
